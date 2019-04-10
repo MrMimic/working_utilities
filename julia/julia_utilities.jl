8f459c88eef4a5c70e8fc7e35b40c42ab79d6755
MrMimic <emeric.dynomant@omictools.com>
2019-04-10 13:05:58 +0200

    Start Julia utilities

--- julia/julia_utilities.jl
+++ julia/julia_utilities.jl
@@ -1,1 +1,35 @@
+module WEB
 
+    # Imports
+    using HTTP, Sockets
+
+    # Exports
+    export get_page
+
+
+    function download_page_data(url)
+        #=
+        This function will download data on the url page
+        Return depends on response status and catched errors
+        =#
+
+        try
+            # Get data
+            response = HTTP.get(url)
+            if typeof(response.status) <: Number && response.status == 200
+                return String(response.body)
+            end
+        # Errors
+        catch error
+            if typeof(error) == Sockets.DNSError
+                println(string("DNS error, please check your URL: ", url))
+            end
+        end
+
+
+
+
+    end
+
+
+end
